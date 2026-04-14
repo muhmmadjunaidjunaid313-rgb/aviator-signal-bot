ae78e4f6f4d016b20d8c9541e2e42af7a1885312
muhmmadjunaidjunaid313-rgb <muhmmadjunaidjunaid313@gmail.com>
2026-04-14 18:07:53 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+import random
+import os
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
+TOKEN = os.getenv("8671120934:AAHpDiUuIL2H3IAaAVeahCP2ruN3X2sWL9A")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("🔥 Aviator Bot Active\nType /signal")
+
+async def signal(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Analyzing... ⏳")
+    
+    num = round(random.uniform(1.2, 5.0), 2)
+    await update.message.reply_text(f"🚀 Next Signal: {num}x")
+
+app = ApplicationBuilder().token(TOKEN).build()
+app.add_handler(CommandHandler("start", start))
+app.add_handler(CommandHandler("signal", signal))
+
+app.run_polling()
